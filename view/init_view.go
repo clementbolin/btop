836729b121c6ca030f00a7a1a1c0af5624edd62d
Bolin <clement.bolin@epitech.eu>
2020-09-18 11:57:35 +0200

    add(master): init tview

--- view/init_view.go
+++ view/init_view.go
@@ -11,10 +11,22 @@ import (
 // 	return box
 // }
 
+func initFlexApp() *tview.Flex {
+	flex := tview.NewFlex().
+			AddItem(tview.NewBox().SetBorder(true).SetTitle("top"), 0, 2, false).
+			AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+					AddItem(tview.NewBox().SetBorder(true).SetTitle("first option"), 0, 1, false).
+					AddItem(tview.NewBox().SetBorder(true).SetTitle("second option"), 0, 2, false).
+					AddItem(tview.NewBox().SetBorder(true).SetTitle("bottom option"), 0, 1, false), 0, 3, false).
+			AddItem(tview.NewBox().SetBorder(true).SetTitle("Right box"), 0, 2, false)
+	
+	return flex
+}
+
 // RunView : init and start default view
 func RunView(title string) {
-	box := tview.NewBox().SetBorder(true).SetTitle(title)
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
-		log.Fatalln(err);
-	}	
+	app := tview.NewApplication()
+	if err := app.SetRoot(initFlexApp(), true).EnableMouse(true).Run(); err != nil {
+		log.Fatalln(err)
+	}
 }
