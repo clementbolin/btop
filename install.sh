9e625dca17e7442310a2d153fa355c0cce6fc14c
Clement Bolin <clement.bolin@epitech.eu>
2020-09-30 19:55:23 +0200

    fix(master): install.sh

--- install.sh
+++ install.sh
@@ -6,30 +6,30 @@ fi
 unamestr="$(uname -s)"
 email=""
 email_confirm=""
-if [ "$unamestr" == 'Linux' ]; then
-    echo "Start install Btop..."
-    make build
-    sudo mv ./bin/btop /usr/local/bin
-    while [ -z $email ] || [ $email != $email_confirm ]
-    do
-        read -p 'Email adress of your commits : ' email
-        read -p 'Confirm your email : ' email_confirm
-    done
-    echo $email > .btop_config
-    mv .btop_config ~/
-    echo "Btop is available on your computer. Try \"btop\" for start"
-elif [ "$unamestr" == 'Darwin' ]; then
-    echo "Start install Btop..."
-    make build
-    sudo mv ./bin/btop /usr/local/bin
-    while [ -z $email ] || [ $email != $email_confirm ]
-    do
-        read -p 'Email adress of your commits : ' email
-        read -p 'Confirm your email : ' email_confirm
-    done
-    echo $email > .btop_config
-    mv .btop_config ~/
-    echo "Btop is available on your computer. Try \"btop\" for start 🚀"
-else
-    echo "Your system is not compatible"
-fi
+case "${unamestr}" in
+    Linux*)
+        echo "Start install Btop..."
+        make build
+        sudo mv ./bin/btop /usr/local/bin
+        while [ -z $email ] || [ $email != $email_confirm ]
+        do
+            read -p 'Email adress of your commits : ' email
+            read -p 'Confirm your email : ' email_confirm
+        done
+        echo $email > .btop_config
+        mv .btop_config ~/
+        echo "Btop is available on your computer. Try \"btop\" for start";;
+    Darwin*)
+        echo "Start install Btop..."
+        make build
+        sudo mv ./bin/btop /usr/local/bin
+        while [ -z $email ] || [ $email != $email_confirm ]
+        do
+            read -p 'Email adress of your commits : ' email
+            read -p 'Confirm your email : ' email_confirm
+        done
+        echo $email > .btop_config
+        mv .btop_config ~/
+        echo "Btop is available on your computer. Try \"btop\" for start 🚀";;
+    *)          echo "Your system is not compatible"
+esac
