6a2ba967c63e4973df73fb49d3d65611b20afa0b
Clement Bolin <clement.bolin@epitech.eu>
2020-10-13 10:46:31 +0200

    fix(master): load .btop_config file

--- modules/gitstatistic/gitStat.go
+++ modules/gitstatistic/gitStat.go
@@ -1,9 +1,8 @@
 package gitstatistic
 
 import (
-	"os"
-	"os/user"
 	"io/ioutil"
+	"os"
 	"strings"
 
 	"github.com/ClementBolin/topGo/modules/gitstatistic/scan"
@@ -12,7 +11,7 @@ import (
 
 // Deleate char at indx
 func delChar(s []rune, index int) []rune {
-    return append(s[0:index], s[index+1:]...)
+	return append(s[0:index], s[index+1:]...)
 }
 
 // clear path string
@@ -20,7 +19,7 @@ func clearPath(path string) string {
 	for i := 0; i != len(path)-2; i++ {
 		c := string(path[i])
 		c1 := string(path[i+1])
-		if (c == "/" && c1 == "/") {
+		if c == "/" && c1 == "/" {
 			path = string(delChar([]rune(path), i))
 		}
 	}
@@ -28,8 +27,8 @@ func clearPath(path string) string {
 }
 
 func readEmail() string {
-	user, _ := user.Current()
-	datas, err := ioutil.ReadFile(user.HomeDir + "/.btop_config")
+	home := strings.Split(os.Getenv("HOME"), "=")
+	datas, err := ioutil.ReadFile(home[0] + "/.btop_config")
 	if err != nil {
 		return ""
 	}
@@ -39,17 +38,16 @@ func readEmail() string {
 // GitStat : GitStat module
 func GitStat() string {
 	var emailFlag string = readEmail()
-	
-	if (emailFlag == "") {
+
+	if emailFlag == "" {
 		var home = strings.Split(os.Getenv("HOME"), "=")
 		return "\n\n\n\n\n\n\n\n\nCreate file name .btop_config in your [red]" + home[0] + "[white] and place email adress use by your commits"
 	}
 
 	path := scan.ScanUniqueFolderGit(os.Getenv("PWD"), emailFlag)
-	if (path[0] == "") {
+	if path[0] == "" {
 		return "\n\n\n\n\nYou are not in git Repository, or we don't find commit from " + emailFlag
 	}
 	path[0] = clearPath(path[0])
 	return ui.CreateTableUI(path, emailFlag)
 }
-	
